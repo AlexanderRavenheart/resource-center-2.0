e24d989b2ac353c9a53db24b735728324b2b8195
Anne Jouke <annejouke@protonmail.com>
2019-12-23 13:25:27 +0100

    main-faq-contact: home page, faq page, style cleanup

--- app/main/urls.py
+++ app/main/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('contacts', views.contacts, name='contacts'),
+    path('faq', views.faq, name='faq'),
 ]
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,26 @@
 from django.http import HttpResponse
 from django.template import loader
 
-def index(request):
-    greeting = 'Hello!'
-    template = loader.get_template('main/index.html')
+def home(request):
+    template = loader.get_template('main/page/home.html')
+    
+    # scObject = Screenshots.objects.filter(ex_name="maps").order_by('-posted')[0:5]
+
     context = {
-        'greeting': greeting,
+        # 'screenshots': scObject,
+    }
+    return HttpResponse(template.render(context, request))
+
+def contacts(request):
+    template = loader.get_template('main/page/contacts.html')
+    context = {
+        'title': 'Contacts',
+    }
+    return HttpResponse(template.render(context, request))
+
+def faq(request):
+    template = loader.get_template('main/page/faq.html')
+    context = {
+        'title': 'FAQ',
     }
     return HttpResponse(template.render(context, request))
